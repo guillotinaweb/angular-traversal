816a3eeb92a39a9caf6ea2a62307f13779917279
Eric BREHAULT <ebrehault@gmail.com>
2017-01-28 21:33:21 +0100

    add material design lite

--- tests/src/app/app.module.ts
+++ tests/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MdlModule } from 'angular2-mdl';
 
 import { TraversalModule } from 'angular-traversal';
 import { Resolver } from 'angular-traversal';
@@ -24,6 +25,7 @@ import { FolderComponent } from './folder/folder.component';
     BrowserModule,
     FormsModule,
     HttpModule,
+    MdlModule,
     TraversalModule.forRoot()
   ],
   entryComponents: [
