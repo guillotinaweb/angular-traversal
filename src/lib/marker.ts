4976ef17246aef72d37e269af8a8c4c424c2e901
Eric BREHAULT <ebrehault@gmail.com>
2017-02-25 23:51:55 +0100

    Supports arrays as marker result

--- src/lib/marker.ts
+++ src/lib/marker.ts
@@ -2,5 +2,5 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export abstract class Marker {
-  abstract mark(context: any): string;
+  abstract mark(context: any): string|string[];
 }
