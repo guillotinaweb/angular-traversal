6cd6825549ec1fe247a15cfc7ceb06e9adbfd947
Eric BREHAULT <ebrehault@gmail.com>
2020-04-10 13:38:59 +0200

    fix tests

---
+++ src/app/file-info-lazy/module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+
+import { FileInfoLazyComponent } from './file-info.component';
+import { ViewMapping } from '../../../projects/traversal/src/public-api';
+
+@NgModule({
+    imports: [],
+    exports: [FileInfoLazyComponent],
+    declarations: [FileInfoLazyComponent],
+    providers: [],
+})
+export class FileInfoLazyModule {
+    static traverserViews: ViewMapping[] = [
+        {name: 'lazy-info', components: {file: FileInfoLazyComponent}},
+    ];
+}
