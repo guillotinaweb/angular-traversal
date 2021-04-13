7bfaf44d65cc9d7ec626ceb0bc2d4f11ff0e0d23
Eric BREHAULT <ebrehault@gmail.com>
2021-04-13 11:14:31 +0200

    provide services in root

--- projects/traversal/src/lib/marker.ts
+++ projects/traversal/src/lib/marker.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 
-@Injectable()
+@Injectable({
+    providedIn: 'root',
+})
 export abstract class Marker {
     abstract mark(context: any): string | string[];
 }
--- projects/traversal/src/lib/resolver.ts
+++ projects/traversal/src/lib/resolver.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
-@Injectable()
+@Injectable({
+    providedIn: 'root',
+})
 export abstract class Resolver {
     abstract resolve(path: string, view: string, queryString?: string): Observable<any>;
 }
